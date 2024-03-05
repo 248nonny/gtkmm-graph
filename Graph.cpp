d847e1bbe24dbfe88c4ca4f690fafebcdc741bc7
248nonny <ronny.cravioto@gmail.com>
2024-03-04 16:18:34 -0800

    added comments and revised code; functionality remains the same

--- Graph.cpp
+++ Graph.cpp
@@ -7,57 +7,53 @@
 
 Graph::Graph() {
 
-    // allocate_data(DEFAULT_TEST_DATA_SIZE,true, true);
-    // printf("lakaka\n");
+    // setup some Gtk parameters.
     set_vexpand(true);
     set_hexpand(true);
     set_margin(0);
+
+    // set minimum size
     set_size_request(grid.pads[PAD_LEFT] + grid.pads[PAD_RIGHT], grid.pads[PAD_TOP] + grid.pads[PAD_BOTTOM]);
+
+    // attach the draw function so it is called whenever the DrawingArea is redrawn.
     set_draw_func(sigc::mem_fun(*this, &Graph::on_draw));
 
+    // update the GUI scale to the macro defined in the hpp file (or with CMake).
     update_gui_scale(GUI_SCALE);
 
 }
 
-Graph::~Graph() {
-}
-
+// this function is called whenever the drawing area is redrawn, so after window resizes and when there's new data.
 void Graph::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height) {
 
+    // set width and height in graph variables.
     grid.width = width;
     grid.height = height;
 
+    // this will run the first time the graph is drawn only.
     if (!grid.runbefore) {
         find_trnfrm();
         get_grid_lines();
 
         cr->set_line_cap(Cairo::Context::LineCap::ROUND);
-        // make_log_data();
-        // make_linear_data();
-
-        // sort_data_x();
     }
 
 
-
+    // this is run whenever the graph is resized.
     if (grid.prev_height != height || grid.prev_width !=width) {
 
+        // find the x,y -> pixels transform for the new dimensions.
         find_trnfrm();
 
-        // printf("%f\n",grid.trnfrm[1](0));
-
-        // cr->move_to(grid.trnfrm[0](grid.xstart), grid.trnfrm[1](grid.ystart));
-        // cr->line_to(grid.trnfrm[0](grid.xstop), grid.trnfrm[0](grid.ystop));
-        // test_trnfrm(cr);
+        // figure out where we want the grid lines to be.
         get_grid_lines();
     }
+
+    // we draw the grid lines
     draw_grid_lines(cr);
     cr->stroke();
 
-    // allocate_data(DEFAULT_TEST_DATA_SIZE,true);
-    // sort_data_x();
-    // make_random_data();
-    // sort_data_x();
+    // we draw the data
     plot_data(cr);
     cr->stroke();
 
@@ -66,15 +62,21 @@ void Graph::on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int heig
     }
 }
 
+
 void Graph::find_trnfrm() {
     // for x:
     long double a,b;
 
+    // we want to find a function where the min. x value is at the leftmost part of the graph, with the right pad as spacing,
+    // and where the max. x value is at the rightmost part of the graph, with the left pad as spacing.
+
     if (grid.x_type == AxisType::LOG) {
+        // the math made sense at the time of writing, and it works without issues :P
         a = (double)(grid.width - grid.pads[PAD_LEFT] - grid.pads[PAD_RIGHT]) / (log10(grid.xstop) - log10(grid.xstart));
-        b = grid.pads[PAD_LEFT] - a * log10(grid.xstart);
+        b = grid.pads[PAD_LEFT] - a * (grid.xstart > 0 ? log10(grid.xstart) : 0.00001);
         grid.trnfrm[0] = [a,b](double xval) {return xval > 0 ? a * log10(xval) + b : b;};
     } else if (grid.x_type == AxisType::LINEAR) {
+        // a simple linear transformation with slope and intercept.
         a = (double)(grid.width - grid.pads[PAD_LEFT] - grid.pads[PAD_RIGHT]) / (grid.xstop - grid.xstart);
         b = grid.pads[PAD_LEFT] - a * grid.xstart;
         grid.trnfrm[0] = [a,b](double xval) {return a * xval + b;};
@@ -82,48 +84,46 @@ void Graph::find_trnfrm() {
         printf("error: Graph.grid.x_type is of unknown type.\n");
     }
 
-    // now for y:
+    // now for y, which is strictly linear:
     a = (double)(grid.height - grid.pads[PAD_TOP] - grid.pads[PAD_BOTTOM]) / (double)(grid.ystart - grid.ystop);
     b = grid.pads[PAD_TOP] - a * grid.ystop;
     grid.trnfrm[1] = [a,b](double yval) {return a * yval + b;};
 }
 
+
 void Graph::get_grid_lines() {
 
-    grid.prev_height = grid.height;
-    grid.prev_width = grid.width;
 
     DLOG(INFO) << "getting grid lines...";
 
     if (grid.x_type == AxisType::LOG) {
 
-        // find main log x lines.
+        // find main log x lines; these will be drawn at each power of 10.
         float logdiff = log10((double)grid.xstop / (double)grid.xstart);
 
         grid.main_x_line_count = ceil(logdiff);
-        if (grid.main_x_line_count >= MAX_MAIN_LINE_COUNT) {
-            // printf("Warning! max line count exceeded, memory issues may be encountered.");
-        }
 
         for (int i = 0; i < grid.main_x_line_count; i++) {
             double &x = grid.main_x_lines[i] = pow(10, ceil(log10(grid.xstart))) * pow(10, i);
-
-            // printf("BL: %d\n",(int)grid.main_x_lines[i]);
         }
 
-        // find sub x lines.
+        // find sub x lines; these will be at multiples of each power of 10 until the next power of ten.
+        // e.g. between 10 and 100 there will be sub lines at 20, 30, 40, 50, 60, 70, 80, and 90.
         grid.sub_x_line_count = 0;
 
+        // find the first and last powers of 10:
         short int firstPow = ceil(log10(grid.xstart));
         short int lastPow = floor(log10(grid.xstop));
 
         // first we loop thru and find the leftmost lines (to the left of the first big line):
         for (int i = 0; i < 10; i++) {
+
+            // find values to the left by multiples of 10^(n-1) where n is the power of the first big line.
             double val = pow(10,firstPow) - (i + 1) * pow(10, firstPow - 1);
 
+            // keep going until we hit the edge of the graph:
             if (val >= grid.xstart) {
                 grid.sub_x_lines[i] = val;
-                // printf("val: %f\n",val);
             } else {
                 grid.sub_x_line_count = i;
                 break;
@@ -132,25 +132,21 @@ void Graph::get_grid_lines() {
 
         // now we add 8 multiples (of sub lines) for each main x line (except the last):
         for (int i = 0; i < grid.main_x_line_count - 1; i++) {
+
             double &mVal = grid.main_x_lines[i];
 
-
-            // why did I write this? great question. am re-reading it now, and no clue tbh :P
-            // I guess I'll just leave it here lol
-            if (mVal == 0) {
-                break;
-            }
-
             for (int i = 0; i < 8; i++) {
-                grid.sub_x_lines[grid.sub_x_line_count] = mVal * (i + 2);
+                grid.sub_x_lines[grid.sub_x_line_count] = mVal * (i + 2); // start with 2 * 10^n, end with 9 * 10^n.
                 grid.sub_x_line_count++;
             }
         }
 
         // now we get the values after the last big line:
         for (int i = 0; i < 10; i++) {
+
             double val = pow(10,lastPow) + (i + 1) * pow(10,lastPow);
 
+            // stop when we hit the right edge of the graph's domain.
             if (val <= grid.xstop) {
                 grid.sub_x_lines[grid.sub_x_line_count] = val;
                 grid.sub_x_line_count++;
@@ -159,29 +155,39 @@ void Graph::get_grid_lines() {
             }
         }
 
+
     } else if (grid.x_type == AxisType::LINEAR) {
-        // printf("gigo\n");
+
+        // find total length of domain
         int xdiff = grid.xstop - grid.xstart;
-        printf("0");
+
+        // find how many main lines we will have:
         grid.main_x_line_count = floor((float)xdiff/(float)grid.main_x_line_increment);
-        printf("1");
-        if (grid.main_x_line_count >= MAX_MAIN_LINE_COUNT) {
+
+        // make sure we don't exceed allocated space for lines array.
+        if (grid.main_x_line_count >= MAX_MAIN_LINE_COUNT - 1) {
             printf("Warning! max line count exceeded, memory issues may be encountered.");
+            return;
         }
 
+        // add an extra line to account for the start and ending edges of the graph,
+        // but only if the xdiff is very close to a multiple of the line increment.
         if (xdiff* 10000 % (int)(grid.main_x_line_increment * 1000) == 0) {
             grid.main_x_line_count += 1;
         }
 
-        // main x lines
+        // find main x lines
         for (int i = 0; i < grid.main_x_line_count || i < 100; i++) {
             grid.main_x_lines[i] = grid.xstart + i * grid.main_x_line_increment;
         }
 
-        // sub x lines
+        // find sub x lines
         for (int i = 0; i < grid.main_x_line_count * grid.x_line_subdiv; i++) {
+
             double val = grid.xstart + i * ((float)grid.main_x_line_increment/(float)grid.x_line_subdiv);
-            if ((int)(val*1000)%(int)(grid.main_x_line_increment * 1000) == 0 && val <= grid.xstop) {
+
+            // store "val" in lines array only if it remains within bounds; otherwise we're done.
+            if ((int)(val*1000) % (int)(grid.main_x_line_increment * 1000) == 0 && val <= grid.xstop) {
                 grid.sub_x_lines[i] = val;
             } else {
                 grid.sub_x_line_count = i;
@@ -189,37 +195,46 @@ void Graph::get_grid_lines() {
             }
         }
 
-        printf("2");
     }
 
-    // now for the x labels
+    // now we store the labels to render next to the lines.
     for (int i = 0; i < grid.main_x_line_count; i++) {
+
         if (grid.main_x_lines[i] == 0) {
+
             grid.x_line_labels[i] = "0";
+
         } else if (log10(grid.main_x_lines[i]) < 0) {
+
             int f = ceil(-1 *log10(grid.main_x_lines[i]));
             grid.x_line_labels[i] = "0.";
+
             for (int i = 0; i < f - 1; i++) {
                 grid.x_line_labels[i].append("0");
             }
+
             grid.x_line_labels[i].append(std::to_string((int)((grid.main_x_lines[i] * pow(10,f) + 0.5))));
+
         } else {
+
             grid.x_line_labels[i] = std::to_string((int)grid.main_x_lines[i]);
+
         }
     }
 
 
     // now for the y lines :o
+    // (same process as for linear x lines)
     int ydiff = grid.ystop - grid.ystart;
     grid.main_y_line_count = (((int)(ydiff*1000) % (int)(grid.main_y_line_increment*1000)) == 0 && (int)(grid.ystart*1000)%(int)(grid.main_y_line_increment*1000) == 0) ? floor((float)ydiff/(float)grid.main_y_line_increment) + 1 : floor((float)ydiff/(float)grid.main_y_line_increment);
 
-    //main y lines:
+    // find main y lines:
     for (int i = 0; i < grid.main_y_line_count; i++) {
         grid.main_y_lines[i] = grid.ystart + i * grid.main_y_line_increment;
     }
 
 
-    // sub y lines:
+    // find sub y lines:
     for (int i = 0; i < grid.main_y_line_count * grid.y_line_subdiv; i++) {
         double val = grid.ystart + i * ((float)grid.main_y_line_increment/(float)grid.y_line_subdiv);
         if ((int)(val*1000)%(int)(grid.main_y_line_increment * 1000) == 0 && val <= grid.ystop) {
@@ -230,11 +245,16 @@ void Graph::get_grid_lines() {
         }
     }
 
-    DLOG(INFO) << "Lines gotten successfully.";
+
+    // record the dimensions that we calculated this for as "prev height" to later check if recalculating all this is necessary. 
+    grid.prev_height = grid.height;
+    grid.prev_width = grid.width;
+
+    DLOG(INFO) << "Lines acquired successfully.";
 }
 
 
-
+// a quick function to test the trnfm variable by directly drawing linear data on the graph.
 void Graph::test_trnfrm(const Cairo::RefPtr<Cairo::Context>& cr) {
 
     float slope = (float)(grid.ystop - grid.ystart) / (float)(grid.xstop - grid.xstart); // y = ax + b
@@ -252,37 +272,37 @@ void Graph::test_trnfrm(const Cairo::RefPtr<Cairo::Context>& cr) {
 
 }
 
+
 void Graph::draw_grid_lines(const Cairo::RefPtr<Cairo::Context>& cr) {
 
+    // set color to grid line color, and set fontsize.
     cr->set_source_rgba(grid.grid_line_rgba[0],grid.grid_line_rgba[1],grid.grid_line_rgba[2],grid.grid_line_rgba[3]);
     cr->set_font_size(grid.fontsize);
 
     // draw main x lines
     for (int i = 0; i < grid.main_x_line_count; i++) {
-        static std::string x_line_labels[MAX_MAIN_LINE_COUNT];
-        
 
-        double &x = grid.main_x_lines[i];
-        draw_v_line(cr, x);
-        
-        cr->move_to(grid.trnfrm[0](x) - 6, grid.trnfrm[1](grid.ystart) + grid.text_offset);
+        draw_v_line(cr, grid.main_x_lines[i]);
+
+        // draw line label
+        cr->move_to(grid.trnfrm[0](grid.main_x_lines[i]) - 6, grid.trnfrm[1](grid.ystart) + grid.text_offset);
         cr->rotate_degrees(grid.text_angle);
-
         cr->show_text(grid.x_line_labels[i]);
-        
-
         cr->rotate_degrees(-grid.text_angle);
     }
 
     // draw main y lines
     for (int i = 0; i < grid.main_y_line_count; i++) {
-        double y = grid.main_y_lines[i];
+        double &y = grid.main_y_lines[i];
         draw_h_line(cr, y);
+
+        // draw line label
         cr->move_to(grid.trnfrm[0](grid.xstart) - grid.text_offset * 3,grid.trnfrm[1](y) + 0.3 * grid.fontsize);
         cr->show_text(std::to_string((int)y));
 
     }
 
+    // set line width and stroke main lines.
     cr->set_line_width(grid.thick_line_width);
     cr->stroke();
 
@@ -296,8 +316,11 @@ void Graph::draw_grid_lines(const Cairo::RefPtr<Cairo::Context>& cr) {
         draw_h_line(cr, grid.sub_y_lines[i]);
     }
 
+    // set line width and stroke sub lines
     cr->set_line_width(grid.thin_line_width);
     cr->stroke();
+
+    // all done!
 }
 
 void Graph::draw_v_line(const Cairo::RefPtr<Cairo::Context>& cr, double x) {
@@ -312,25 +335,30 @@ void Graph::draw_h_line(const Cairo::RefPtr<Cairo::Context>& cr, double y) {
 
 
 void Graph::plot_data(const Cairo::RefPtr<Cairo::Context>& cr) {
+
     DLOG(INFO) << "\nplotting graph data. number of data sets: " << data.size();
+
+    // plot each data set.
     for (int i = 0; i < data.size(); i++) {
         double range = (grid.xstop - grid.xstart) * 0.01;
 
         DLOG(INFO) << "plotting data set " << i+1 << "/" << data.size() << " of size " << data[i].size();
 
+        // check if data exists in current dataset:
         if (data[i].size() > 0) {
+
+            // set line width and line color.
             cr->set_line_width(grid.data_line_width);
             int rgba_i = i % NUM_COLOURS;
-            // printf("rgba_i: %d, r:%f, g:%f, b:%f, a:%f\n\n",rgba_i,grid.data_line_rgba[rgba_i][0],grid.data_line_rgba[rgba_i][1],grid.data_line_rgba[rgba_i][2],grid.data_line_opacity);
-            
             cr->set_source_rgba(grid.data_line_rgba[rgba_i][0],grid.data_line_rgba[rgba_i][1],grid.data_line_rgba[rgba_i][2],grid.data_line_opacity);
             
-            // cr->move_to(0, 0);
             DLOG(INFO) << "data exists in this data set. finding first plottable point.";
 
             double x0;
             double y0;
 
+            // find coordinates of first point; unchanged if first data point is within graph bounds,
+            // but along x or y axis if values are out of range.
             if (data[i][0][0] <= grid.xstart) {
                 x0 = grid.trnfrm[0](grid.xstart);
             } else if (data[i][0][0] >= grid.xstop) {
@@ -347,8 +375,10 @@ void Graph::plot_data(const Cairo::RefPtr<Cairo::Context>& cr) {
                 y0 = grid.trnfrm[1](data[i][0][1]);
             }
 
+            // move to first point
             cr->move_to(x0, y0);
 
+            // draw a line to each data point while staying within graph bounds.
             for (int j = 0; j < data[i].size(); j++) {
                 cr->line_to(
                     grid.trnfrm[0]((data[i][j][0] >= grid.xstart && data[i][j][0] <= grid.xstop) ? data[i][j][0] : (data[i][j][0] >= grid.xstop) * grid.xstop + (data[i][j][0] < grid.xstop) * grid.xstart),
@@ -356,16 +386,18 @@ void Graph::plot_data(const Cairo::RefPtr<Cairo::Context>& cr) {
                 );
             }
             
+            // stroke the data lines.
             cr->stroke();
             
 
         }
     }
 
-    cr->stroke();
     DLOG(INFO) << "graph data plotted successfully.";
 }
 
+
+// fill data with randomness.
 void Graph::make_random_data(int data_slot) {
     DLOG(INFO) << "making random data for graph in data slot " << data_slot << ".";
     // allocate_data(DEFAULT_TEST_DATA_SIZE);
@@ -379,6 +411,7 @@ void Graph::make_random_data(int data_slot) {
     
 }
 
+// fill data with log. curve.
 void Graph::make_log_data(int data_slot) {
     DLOG(INFO) << "making log data for graph in data slot " << data_slot << ".";
 
@@ -397,6 +430,7 @@ void Graph::make_log_data(int data_slot) {
     }
 }
 
+// fill data with straight line.
 void Graph::make_linear_data(int data_slot) {
     DLOG(INFO) << "making linear data for graph in data slot " << data_slot << ".";
     // allocate_data(DEFAULT_TEST_DATA_SIZE);
@@ -416,76 +450,29 @@ void Graph::make_linear_data(int data_slot) {
 }
 
 
-
-void Graph::sort_data_x(int data_slot) {
-    DLOG(INFO) << "sorting data in slot " << data_slot << " by x axis value.";
-    GraphDataSet tempData;
-    tempData.resize(DEFAULT_TEST_DATA_SIZE);
-    for (int i = 0; i < data[data_slot].size(); i++) {
-        // tempData.data[i].resize(data[data_slot][i].size());
-        for (int j = 0; j < 1; j++) {
-            tempData[i][j] = data[data_slot][i][j];
-        }
-    }
-
-    // allocate_data(DEFAULT_TEST_DATA_SIZE,true);
-    data[data_slot].resize(DEFAULT_TEST_DATA_SIZE);
-    
-    for (int i = 0; i < tempData.size(); i ++) {
-    	
-        if (i == 0) {
-        	data[data_slot][i][0] = tempData[i][0];
-        	data[data_slot][i][1] = tempData[i][1];
-            
-        } else {
-        	for (int ii = i; ii >= 0; ii --) {
-            	
-                
-              	if (ii == 0) {
-                	data[data_slot][ii][0] = tempData[i][0];
-                    data[data_slot][ii][1] = tempData[i][1];
-                } else if (data[data_slot][ii - 1][0] > tempData[i][0]) {
-                	data[data_slot][ii][0] = data[data_slot][ii - 1][0];
-                    data[data_slot][ii][1] = data[data_slot][ii - 1][1];
-                } else {
-                	data[data_slot][ii][0] = tempData[i][0];
-                    data[data_slot][ii][1] = tempData[i][1];
-                    break;
-                }
-
-            }
-        }
-        
-    }
-    DLOG(INFO) << "Success.";
-}
-
 void Graph::write_data(GraphDataSet input_data, int data_slot) {
+
     DLOG(INFO) << "writing graph data in slot " << data_slot << ".";
 
-    if (data.size() < data_slot + 1) {
-        data.resize(data_slot + 1);
-    }
-
+    // remove all data first, then write into variable
     data[data_slot].resize(0);
     data[data_slot].reserve(input_data.size());
+
+    // write data
     for (int i = 0; i < input_data.size(); i++) {
         data[data_slot].push_back({input_data[i][0], input_data[i][1]});
-        // printf("x: %f, y: %f\n",input_data[i][0],input_data[i][1]); // for testing 
     }
+
     DLOG(INFO) << "data written successfully; queuing redraw.";
     queue_draw();
 }
 
-// void Graph::allocate_data(int size, bool set_to_zero, bool force_allocate) {
-//     allocate_GraphData(size, set_to_zero, data, force_allocate);
-// }
-
-
 void Graph::update_gui_scale(double scale) {
 
+    // figure out factor of change between current scale and new scale.
     double factor = scale / grid.current_scale;
 
+    // scale everything accordingly.
     grid.fontsize = grid.fontsize * factor;
     grid.text_offset = grid.text_offset * factor;
 
@@ -495,5 +482,6 @@ void Graph::update_gui_scale(double scale) {
 
     grid.data_line_width = grid.data_line_width * factor;
 
+    // set current scale
     grid.current_scale = scale;
 }
--- Graph.hpp
+++ Graph.hpp
@@ -1,26 +1,22 @@
 #pragma once
 
-#include "cairomm/context.h"
-#include "cairomm/refptr.h"
-#include "gtkmm/enums.h"
-#include <gtkmm/drawingarea.h>
+#include <vector>
+#include <gtkmm.h>
 
 
 
+// This GUI_SCALE macro should ideally be defined in a CMakeLists.txt file; if not it defaults to 1.
 #ifndef GUI_SCALE
 #define GUI_SCALE 1
 #endif
 
+
 // max params for graphs.
 const short int MAX_MAIN_LINE_COUNT = 100;
 const short int MAX_SUB_LINE_COUNT = 100;
-// #define MAX_MAIN_LINE_COUNT 100
-// #define MAX_SUB_LINE_COUNT 100
+const short int MAX_DATAPOINTS = 200;
 
-const short int MAX_DATAPOINTS = 50;
-// #define MAX_DATAPOINTS 50
-
-// for pads
+// for pads on the edges of the drawing area.
 enum class GraphPad
 {
     PAD_TOP,
@@ -28,35 +24,37 @@ enum class GraphPad
     PAD_BOTTOM,
     PAD_LEFT
 };
+
+// to define whether the x axis is linear or logarithmic.
 enum class AxisType
 {
     LOG,
     LINEAR
 };
 
+
 #define PAD_TOP 0
 #define PAD_RIGHT 1
 #define PAD_BOTTOM 2
 #define PAD_LEFT 3
 
-
 #define NUM_COLOURS 5
 
-#include <vector>
 
-// - data (contains datasets)
-// --- dataset (contains x,y pairs)
-// ----- x,y pair (double)
+// - GraphData (contains GraphDataSets)
+// --- GraphDataSet (contains x,y pairs)
+// ----- x,y pair (vector of doubles with size 2)
 using GraphDataSet = std::vector<std::vector<double>>;
 using GraphData = std::vector<GraphDataSet>;
 
 
-// struct GraphData {
-//     std::vector<std::vector<std::vector<double>>> data;
-// };
+#define DEFAULT_TEST_DATA_SIZE 200
 
-#define DEFAULT_TEST_DATA_SIZE 50
+// Future Me here: When I wrote this, I was still exploring c++, so I must say the structure of all that is below,
+// and all that is in the Graph.cpp file is rather unorthodox and at times confusing. I apologize in advance :P
 
+
+// a struct with some parameters for the graph.
 struct Grid {
     bool runbefore = false;
 
@@ -74,6 +72,7 @@ struct Grid {
     double ystart = -40;
     double ystop = 100;
 
+    // variables that store the coordinates to draw lines at.
     double main_x_lines[MAX_MAIN_LINE_COUNT]; // main lines determine tick marks, sub lines are for visuals only.
     int main_x_line_count = 0;
     std::string x_line_labels[MAX_MAIN_LINE_COUNT];
@@ -90,76 +89,94 @@ struct Grid {
     double main_y_line_increment = 20;
     double y_line_subdiv = 4;
 
-    //for linear only:
+    //for linear x axis only:
     double main_x_line_increment = 0.1;
     double x_line_subdiv = 5;
 
-    int pads[4] = {16,16,50,50}; // goes like compass, element 0 is up, 1 is right, 2 is bottom, 3 is left.
-    // int pads[4] = {0,0,0,0};
+    // goes like a compass, element 0 is top, 1 is right, 2 is bottom, 3 is left.
+    // this relationship is defined in a series of "#define" statements at the top of the file.
+    int pads[4] = {16,16,50,50};
 
+    // a set of functions that map the x-y variable space to literal pixels in the DrawingArea;
+    // this is used extensively when drawing lines on the graph later.
     std::function<double(double)> trnfrm[2]; // first element is x trnfm, second is y.
 
     int fontsize = 12;
 
-    double current_scale = 1;
+    double current_scale = 1; // used with GUI_SCALE to scale graph elements.
 
-    float grid_line_rgba[4] = {0.7,0.7,0.7, 0.6};
+    float grid_line_rgba[4] = {0.7,0.7,0.7, 0.6}; // define color of the grid lines.
 
-    float data_line_width = 2;
+    float data_line_width = 2; // width of grid line in px (will be scaled according to GUI_SCALE).
 
     float data_line_opacity = 0.7;
 
-    // std::vector<std::vector<float>> data_line_rgba = {
-    const double data_line_rgba[NUM_COLOURS][3] = {
+    // colors to use for each dataset (multiple can be graphed at a time, so this helps distinguish them).
+    double data_line_rgba[NUM_COLOURS][3] = {
         {1,0.27058,0}, // orange-y
         {0.114, 0.929, 0}, //green
         {0, 0.914, 0.929},
         {0.604, 0, 0.929},
         {0.929, 0, 0}
     };
-    // float data_line_rgba[4] = {1,0.27058,0,0.7};
 
+    // text size and offset for axis numbering.
     int text_offset = 10;
     int text_angle = 60;
+
+    // line widths for graph grid lines.
     int thick_line_width = 4;
     int thin_line_width = 2;
 };
 
-
-void allocate_GraphData(int size, bool set_to_zero,GraphData &data, bool force_allocate = false);
-
+// The Graph class, inheriting from the Gtk DrawingArea.
 class Graph : public Gtk::DrawingArea {
 public:
-    Graph();
-    virtual ~Graph();
 
+    Graph();
+
+    // create grid parameters and data variables.
     Grid grid;
     GraphData data;
     int data_index;
+
+    // setter function to write new data to graph. Graph.queue_draw() should be called after to render the new data
+    // (this is inherited from the DrawingArea).
     void write_data(GraphDataSet input_data, int data_slot = 0);
 
+    // setter function to update the graphics scale; useful for high dpi screens.
     void update_gui_scale(double scale = 1);
 
-
+    // functions to test the graph by creating certain types of data; these were used extensively for testing.
     void make_random_data(int data_slot = 0);
     void make_sine_data(int data_slot = 0);
     void make_log_data(int data_slot = 0);
     void make_linear_data(int data_slot = 0);
 
 protected:
+
+    // the function that gets called every time the DrawingArea is asked to redraw iteself.
     void on_draw(const Cairo::RefPtr<Cairo::Context>& cr, int width, int height);
+
+    // a function that determines x and y values for grid lines based on the domain and range specified in "grid" (ystart, xstart, ystop, xstop).
     void get_grid_lines();
+
+    // a function that finds the maps from x,y in the space of the data to literal pixels (based on height and width of widget).
     void find_trnfrm();
+
+    // this was used to test whether the trnfrm found is accurate; it is no longer necessary,
+    // but I'm keeping it here for sentimental value :D
     void test_trnfrm(const Cairo::RefPtr<Cairo::Context>& cr);
+
+    // this function reads stored x and y values for grid lines and draws them.
     void draw_grid_lines(const Cairo::RefPtr<Cairo::Context>& cr);
     
+    // functions to draw vertical or horizontal lines within the bounds of the graph.
     void draw_v_line(const Cairo::RefPtr<Cairo::Context>& cr, double x);
-    void draw_h_line(const Cairo::RefPtr<Cairo::Context>& cr, double x);
+    void draw_h_line(const Cairo::RefPtr<Cairo::Context>& cr, double y);
 
+    // This function draws a line connecting all the datapoints in the graph's stored data (it plots the data :o)
     void plot_data(const Cairo::RefPtr<Cairo::Context>& cr);
 
-
-
-    void sort_data_x(int data_slot);
 };
 
